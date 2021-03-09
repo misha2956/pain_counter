fb8b8882fd87f195111c74c9ba0ba363d1e4a7b4
misha2956 <misharobot03@gmail.com>
2021-03-09 22:28:20 +0200

    Add files via upload

---
+++ pain_counter.py
@@ -0,0 +1,9 @@
+import os
+
+count = 0
+for root, dirs, files in os.walk(os.path.curdir):
+    for file_str in files:
+        if file_str.endswith(".py"):
+            count += open(os.path.join(root, file_str)).read().count('\n')
+
+print(f"You wrote {count} lines of code in total.")
