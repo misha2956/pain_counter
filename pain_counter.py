6f37cb555c49270adb307a5b6488b1dc6c9d279d
misha2956 <misharobot03@gmail.com>
2021-03-09 22:34:17 +0200

    Update pain_counter.py

--- pain_counter.py
+++ pain_counter.py
@@ -3,7 +3,7 @@ import os
 count = 0
 for root, dirs, files in os.walk(os.path.curdir):
     for file_str in files:
-        if file_str.endswith(u".py"):
+        if file_str.endswith(".py"):
             count += open(os.path.join(root, file_str)).read().count('\n')
 
 print(f"You wrote {count} lines of code in total.")
